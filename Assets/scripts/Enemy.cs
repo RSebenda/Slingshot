7b460d96b29e84eead9340b5dd91370ad313d20d
VFS\rsebenda <rsebenda@gmail.com>
2017-03-08 11:38:16 -0800

    added DoTween and movement to enemies

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,7 +1,39 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 using System.Collections;
 
 public class Enemy : BaseUnit {
 
+    int moveXOffset = 1;
+    int moveYOffset = 1;
+    float moveSpeed = 0.5f;
+
+    void Start()
+    {
+        Move();
+    }
+
+    void Update()
+    {
+
+
+    }
+
+
+
+    Vector2 FindNextPosition()
+    {
+        float randomX = Random.Range(transform.position.x + -moveXOffset, transform.position.x + moveXOffset); 
+        Vector2 newPos = new Vector2(randomX, transform.position.y - moveYOffset);
+        return newPos;
+    }
+
+
+    void Move()
+    {
+        Vector2 newPos = FindNextPosition();
+        transform.DOMove(newPos, moveSpeed).SetSpeedBased(true).SetEase(Ease.Linear).OnComplete(Move);
+        transform.DORotate(newPos, moveSpeed).SetSpeedBased(true).SetEase(Ease.Linear);
+    }
 
 }
